d28e06916feb6ea1e110b9f4e036f32c3df352a8
Sanif Shahzad <110146978+0sanif@users.noreply.github.com>
2024-03-20 21:03:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//Treat all code as new Js version
+
+//alert(3+2); // Will not work, we are using Nodejs not browser
+
+console.log(3+
+    3); // Code readablity should be highnode 
+console.log("sanif");
+
+/*
+number=>2 to the power 53
+bigInt
+string=>""
+boolean=>true or false
+undefined=>undefined
+null=>object
+*/
+console.log(typeof null);
+console.log(typeof "sanif");
+console.log(typeof 999999999999999999999);
+console.log(typeof false);
