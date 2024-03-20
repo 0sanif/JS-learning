5e1ac381d1dacf9804f1e82cdcfc79f6cc9abb2d
Sanif Shahzad <110146978+0sanif@users.noreply.github.com>
2024-03-20 20:56:57 +0000

    variales

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1445
+let accountEmail = "sanif@gmail.com"
+var accountPassword = "1234"
+accountCity ="Srinagar"
+let accountState;
+
+//accountId = 2 // Not Allowed
+accountEmail="hh@gmail.com"
+accountPassword="33244"
+accountCity="Delhi"
+console.log(accountId);
+/*
+Not to use var 
+Because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
