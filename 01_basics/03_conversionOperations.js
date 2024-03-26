ef8453ccd7d4ed600693bb27de47af81691a3043
Sanif Shahzad <110146978+0sanif@users.noreply.github.com>
2024-03-26 20:45:36 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,15 @@
+let score = true;
+
+//console.log(typeof(score));
+let valueOfScore = Number(score);
+//console.log(typeof(valueOfScore));
+//console.log(valueOfScore);
+
+//"33"=> 33
+//"33abc"=> NaN.
+let something = ""
+let so = Boolean(something)
+console.log(so);
+console.log(typeof(so));
+//1=>true > boolean
+//""=>false > boolean
