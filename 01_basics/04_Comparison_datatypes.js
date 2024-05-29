5bbc5690749ee084344f7dc7cd84ea1981c6ec58
Sanif Shahzad <110146978+0sanif@users.noreply.github.com>
2024-05-29 08:06:20 +0000

    Heap and stack

--- 01_basics/04_Comparison_datatypes.js
+++ 01_basics/04_Comparison_datatypes.js
@@ -7,23 +7,48 @@
 
 //-----DataTypes Of JavaScript-----
 //Primitive----String, Number, Boolean, Null, undefined, Symbol, BigInt
-const score = 100;
-const scoreValue=100.3;//No Need of explicitly define the type of the variable
-console.log(scoreValue);
+// const score = 100;
+// const scoreValue=100.3;//No Need of explicitly define the type of the variable
+// console.log(scoreValue);
 
-const isLoggedIn= false;
-const outsideTemp=null;
-console.log(isLoggedIn);
-console.log(outsideTemp);
+// const isLoggedIn= false;
+// const outsideTemp=null;
+// console.log(isLoggedIn);
+// console.log(outsideTemp);
 
-const id = Symbol('123');
-console.log(id);
+// const id = Symbol('123');
+// console.log(id);
 
 //Reference Type/Non-Primitive----Array, Objects, Functions
 
-const heroes =["Shakhtiman", "Batman", "Superman"];
-//console.log(heroes);
-let myObj={
-    name:"hitesh",
-    age:22
+// const heroes =["Shakhtiman", "Batman", "Superman"];
+// //console.log(heroes);
+// let myObj={
+//     name:"hitesh",
+//     age:22
+// }
+
+//Stack And Heap 
+//Stack gets the copy of a datatype. Stacks are only for primitive dataTypes
+let myName = "Sanif";
+let anotherName = myName;// myName assigned in anothername---used stack memory
+console.log(anotherName);
+anotherName = "Basit";
+console.log(myName);//Original value is still there
+console.log(anotherName);
+
+//In Stack value will not get changed
+
+//Heap gets the reference of a datatype. 
+
+let userOne = {
+    email : "user@gmail.com",
+    upi:"user@hdfc"
 }
+let userTwo = userOne;
+console.log(userTwo);
+userTwo.email = "sanif@gmail.com";
+console.log(userTwo);
+console.log(userOne);//Here value changed in userOne when we changed it in userTwo then changes also reflected in userOne
+
+
